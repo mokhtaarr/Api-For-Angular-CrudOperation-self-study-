9c7af5c424ba83a00db28245891bd9c21cb83df6
Mokhtar Ehab <ehabmokhtar32@gmail.com>
2023-07-08 16:54:27 +0200

    finishing the small example for basic crudOPeration for angular v16 with .net6

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _db;
+
+        public SuperHeroController(DataContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return Ok(await _db.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHeroes(SuperHero hero)
+        {
+            _db.SuperHeroes.Add(hero);
+            await _db.SaveChangesAsync();
+
+            return Ok(await _db.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHeroes(SuperHero hero)
+        {
+            var dbHero = await _db.SuperHeroes.FindAsync(hero.Id);
+            if (dbHero == null)  
+                return BadRequest("hero not found");
+
+            dbHero.Name = hero.Name;
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.Place = hero.Place;
+
+            await _db.SaveChangesAsync();
+
+            return Ok(await _db.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var dbHero = await _db.SuperHeroes.FindAsync(id);
+            if (dbHero == null) 
+                return BadRequest("hero not found");
+
+            _db.SuperHeroes.Remove(dbHero);
+            await _db.SaveChangesAsync();
+
+            return Ok(await _db.SuperHeroes.ToListAsync());
+        }
+
+    }
+}
